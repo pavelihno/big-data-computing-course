ce1459460d7465bf3e02e2a7941e0f373c27ed5b
pavelihno <crazzyyellow02@gmail.com>
2025-04-09 21:30:47 +0200

    2 round version implemented. local space analysis required

--- src/G23HW1.java
+++ src/G23HW1.java
@@ -57,16 +57,25 @@ public class G23HW1 {
         return Math.max(averageSqauredDistanceA, averageSquaredDistanceB);
     }
 
-    public static void MRPrintStatistics(JavaPairRDD<Vector, String> points, ArrayList<Vector> centers) {
+    public static void MRPrintStatistics(JavaPairRDD<Vector, String> points, ArrayList<Vector> centers, int L) {
         /*
          * Computes and prints statistics for each cluster
          */
 
         // Count group A and B for each cluster
-        HashMap<Integer, Tuple2<Integer, Integer>> counts = new HashMap<>();
+        HashMap<Integer, Tuple2<Long, Long>> counts = new HashMap<>();
 
+        /* Version 1: 1 round solution.
+         * 
+         * Round 1
+         * Map: (Point(x, y), group) -> (cluster, (countA, countB))
+         * Reduce: (cluster, [(countA1, countB1), (countA2, countB2)]) -> (cluster, (countA1 + countA2, countB1 + countB2))
+         *
+         * Local space = O(N)
+        */
+        /*
         counts.putAll(points.mapToPair(tuple -> {
-            /* Local space = O(1) */
+            // Local space = O(1)
             Vector point = tuple._1();
             String group = tuple._2();
             int cluster = -1;
@@ -79,21 +88,84 @@ public class G23HW1 {
                 }
             }
 
-            int countA = group.equals("A") ? 1 : 0;
-            int countB = group.equals("B") ? 1 : 0;
+            long countA = group.equals("A") ? 1L : 0L;
+            long countB = group.equals("B") ? 1L : 0L;
 
             return new Tuple2<>(cluster, new Tuple2<>(countA, countB));
         }).reduceByKey((t1, t2) -> {
-            /* Local space (worst case: all points belong to a single cluster) = O(n) */
+            // Local space (worst case: all points belong to a single cluster) = O(N)
+            return new Tuple2<>(t1._1() + t2._1(), t1._2() + t2._2());
+        }).collectAsMap());
+        */
+
+        /* Version 2: 2 rounds solution. 
+         * 
+         * Round 1
+         * Map: (i, (Point(x, y), group)) -> (i % mod, {cluster: (countA, countB)})
+         * Reduce: (j, [{cluster: (countA1, countB1)}, {cluster: (countA2, countB2)}]) -> (j, {cluster: (countA1 + countA2, countB1 + countB2)})
+
+         * Round 2
+         * Map: (j, {cluster: (countA, countB)}) -> (cluster, (countA, countB))
+         * Reduce: (cluster, [(countA1, countB1), (countA2, countB2)]) -> (cluster, (countA1 + countA2, countB1 + countB2))
+         * 
+         * Local space =
+        */
+
+        counts.putAll(points.zipWithIndex().mapToPair(tuple -> {
+            // Local space = O(1)
+            Long index = tuple._2();
+            Vector point = tuple._1()._1();
+            String group = tuple._1()._2();
+            int cluster = -1;
+            double bestDist = Double.MAX_VALUE;
+            for (int i = 0; i < centers.size(); i++) {
+                double dist = Vectors.sqdist(point, centers.get(i));
+                if (dist < bestDist) {
+                    bestDist = dist;
+                    cluster = i;
+                }
+            }
+
+            long countA = group.equals("A") ? 1L : 0L;
+            long countB = group.equals("B") ? 1L : 0L;
+
+            HashMap<Integer, Tuple2<Long, Long>> localCounts = new HashMap<>();
+
+            localCounts.put(cluster, new Tuple2<>(countA, countB));
+
+            return new Tuple2<>(index % L, localCounts);
+        }).reduceByKey((map1, map2) -> {
+            // Local space = O(N / L)
+
+            // Collect all counts for each cluster in one map (map1)
+            for (Integer cl : map2.keySet()) {
+                Tuple2<Long, Long> localCounts = map2.get(cl);
+                if (map1.containsKey(cl)) {
+                    Tuple2<Long, Long> existing = map1.get(cl);
+                    map1.put(cl, new Tuple2<>(existing._1() + localCounts._1(), existing._2() + localCounts._2()));
+                } else {
+                    map1.put(cl, localCounts);
+                }
+            }
+            return map1;
+        }).flatMapToPair(tuple -> {
+            // Local space =
+            return tuple._2().entrySet().stream()
+                    .map(entry -> new Tuple2<>(
+                        entry.getKey(),
+                        entry.getValue()
+                    ))
+                    .iterator();
+        }).reduceByKey((t1, t2) -> {
             return new Tuple2<>(t1._1() + t2._1(), t1._2() + t2._2());
         }).collectAsMap());
 
         // Print statistics for each cluster
         for (int i = 0; i < centers.size(); i++) {
             Vector center = centers.get(i);
-            Tuple2<Integer, Integer> tuple = counts.get(i);
-            int na = (tuple != null) ? tuple._1() : 0;
-            int nb = (tuple != null) ? tuple._2() : 0;
+            Tuple2<Long, Long> tuple = counts.get(i);
+            long na = (tuple != null) ? tuple._1() : 0L;
+            long nb = (tuple != null) ? tuple._2() : 0L;
             System.out.printf(
                     "i = %d, center = (%.6f,%.6f), NA%d = %d, NB%d = %d\n",
                     i, center.apply(0), center.apply(1), i, na, i, nb);
@@ -155,7 +227,7 @@ public class G23HW1 {
             System.out.printf("Delta(U,C) = %.6f\n", standardObjective);
             System.out.printf("Phi(A,B,C) = %.6f\n", fairObjective);
 
-            MRPrintStatistics(inputPoints, centers);
+            MRPrintStatistics(inputPoints, centers, L);
 
         } catch (Exception e) {
             e.printStackTrace();
